eda079c82564e304b39d5a7dfdd84d066f47d160
LuYan <imluyan@qq.com>
2017-03-23 19:01:08 +0800

    unfinished

--- app.py
+++ app.py
@@ -1,14 +1,12 @@
 # -*- coding: utf-8 -*-
+import sqlite3
+import ssl
 import urllib.request, urllib.error
 from bs4 import BeautifulSoup
-import ssl
 import re
-import sqlite3
-
 
 conn = sqlite3.connect('models.sqlite')
 cur = conn.cursor()
-
 cur.execute('DROP TABLE IF EXISTS Models')
 cur.execute('CREATE TABLE Models (user_id TEXT PRIMARY KEY, name TEXT, '
             'info_card_url TEXT, album_list_url TEXT, the_album_url TEXT, the_pic_url TEXT)')
@@ -45,4 +43,50 @@ except urllib.error as e:
         print(e.reason)
 
 conn.commit()
+
+cur.execute('SELECT info_card_url FROM Models WHERE album_list_url is NULL')
+info_url_lst = cur.fetchall()
+for info_url in info_url_lst:
+    go_info_url = info_url[0]
+    try:
+        request = urllib.request.Request(url=go_info_url, headers=header)
+        response = urllib.request.urlopen(url=request, context=ctx)
+        html = response.read()
+        soup = BeautifulSoup(html, 'html.parser')
+        tags = soup('a')
+        for tag in tags:
+            try:
+                content = tag.contents[0]
+                if content == '相册':
+                    album_list_url = tag.get('href', None)
+                    comp_album_list_url = 'https:' + album_list_url
+                    print(comp_album_list_url)
+                    cur.execute('UPDATE Models SET album_list_url = ? WHERE info_card_url = ?',
+                                (comp_album_list_url, go_info_url))
+            except:
+                pass
+    except urllib.error as e:
+        if hasattr(e, 'code'):
+            print(e.code)
+        if hasattr(e, 'reason'):
+            print(e.reason)
+
+conn.commit()
+
+cur.execute('SELECT album_list_url FROM Models WHERE the_album_url is NULL')
+the_album_url = cur.fetchall()
+for album_url in the_album_url:
+    go_album_url = album_url[0]
+    try:
+        request = urllib.request.Request(url=go_album_url, headers=header)
+        response = urllib.request.urlopen(url=request, context=ctx)
+        html = response.read()
+        soup = BeautifulSoup(html, 'html.parser')
+        print(soup.prettify())
+    except urllib.error as e:
+        if hasattr(e, 'code'):
+            print(e.code)
+        if hasattr(e, 'reason'):
+            print(e.reason)
+
 conn.close()
